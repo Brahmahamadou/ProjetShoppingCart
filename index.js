8451eb3487bee9902d64b4d706f679f55d86316b
Brahmahamadou <124585116+Brahmahamadou@users.noreply.github.com>
2024-01-03 04:05:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let myCard = document.getElementById("card");
+let coffre = document.getElementById("coffre")
+myCard.addEventListener('mouseenter', function () {
+    coffre.classList.remove("hidden");
+  });
